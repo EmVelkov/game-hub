62e8948695264f7f7ec1efdffbc01c550425ee2a
Emiliyan Velkov <emiliyanweb@gmail.com>
2023-12-25 23:47:01 +0200

    Display genres

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/free_logos_dribbble_ph.jpg";
+import logo from "../assets/vintage-badge-hand-holding-joystick-vector-illustration-round-label-with-gamepad_74855-11224.avif";
 import ColorModeSwitch from "./colorModeSwitch";
 
 const NavBar = () => {
 	return (
 		<HStack justifyContent="space-between" padding="10px">
-			<Image src={logo} boxSize="60px"></Image>
+			<Image src={logo} boxSize="60px" borderRadius='36px'></Image>
 			<ColorModeSwitch></ColorModeSwitch>
 		</HStack>
 	);
