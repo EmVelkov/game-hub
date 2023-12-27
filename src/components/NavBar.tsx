d6bea988e898d468f05c1ec1f0f2e9a6237d22aa
Emiliyan Velkov <emiliyanweb@gmail.com>
2023-12-27 10:59:13 +0200

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/vintage-badge-hand-holding-joystick-vector-illustration-round-label-with-gamepad_74855-11224.avif";
 import ColorModeSwitch from "./colorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
 	return (
-		<HStack justifyContent="space-between" padding="10px">
-			<Image src={logo} boxSize="60px" borderRadius='36px'></Image>
+		<HStack padding="10px">
+			<Image src={logo} boxSize="60px" borderRadius="36px"></Image>
+			<SearchInput></SearchInput>
 			<ColorModeSwitch></ColorModeSwitch>
 		</HStack>
 	);
