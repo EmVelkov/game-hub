c9180e8e631e1744e63035bfe9493c22da9905f6
Emiliyan Velkov <emiliyanweb@gmail.com>
2024-05-04 12:48:58 +0300

    Fix game trailers loading and view

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -1,9 +1,9 @@
-import Trailer from "./../entities/Trailer";
 import { useQuery } from "@tanstack/react-query";
+import Trailer from "../entities/Trailer";
 import ApiClient from "../services/api-client";
 
 const useTrailers = (gameId: number) => {
-	const apiClient = new ApiClient<Trailer>(`games/${gameId}/movies`);
+	const apiClient = new ApiClient<Trailer>(`/games/${gameId}/movies`);
 
 	return useQuery({
 		queryKey: ["trailers", gameId],
