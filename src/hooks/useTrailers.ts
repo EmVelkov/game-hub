3566ed0ccc91692001a3e9d19e8955c026323ee2
Emiliyan Velkov <emiliyanweb@gmail.com>
2024-05-16 19:05:41 +0300

    Remove line creating ApiClient instance in useTrailers.ts due to incorrect URL

--- src/hooks/useTrailers.ts
+++ src/hooks/useTrailers.ts
@@ -3,7 +3,7 @@ import Trailer from "../entities/Trailer";
 import ApiClient from "../services/api-client";
 
 const useTrailers = (gameId: number) => {
-	const apiClient = new ApiClient<Trailer>(`/games/${gameId}/movies`);
+	const apiClient = new ApiClient<Trailer>(`games/${gameId}/movies`);
 
 	return useQuery({
 		queryKey: ["trailers", gameId],
